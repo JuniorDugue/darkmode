c4e09d8bfd5cfb8f1aad8dfcc65136a4ece3f783
Junior Dugue <jrdugue@gmail.com>
2021-01-06 12:05:07 -0500

    added structure to change theme of website imported required libaries, wrapped app with themeprovider after importing it from styled components, passed object setting mode to dark, imported createGlobalstyle to change theme of site and wrote a function to change bg and font color accordingly

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { ThemeProvider, createGlobalStyle } from "styled-components";
 import App from "./App";
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    background-color: ${(props) =>
+      props.theme.mode === "dark" ? "#111" : "#eee"};
+    color: ${(props) => (props.theme.mode === "dark" ? "#eee" : "#111")};
+  }
+`;
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={{ mode: "dark" }}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   rootElement
 );
